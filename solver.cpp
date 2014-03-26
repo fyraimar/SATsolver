3078a3bf6d1e0c3c3f499abc90647a2ce5801a8a
fyraimar <fyraimar@gmail.com>
2014-03-26 21:59:35 +0800

    add input from file

--- solver.cpp
+++ solver.cpp
@@ -1,16 +1,19 @@
 #include "matrix.h"
 
 #include <iostream>
+#include <fstream>
 
 int main() {
   int var_num, sen_num;
-  std::cin >> var_num >> sen_num;
+  std::ifstream in;
+  in.open("testdata");
+  in >> var_num >> sen_num;
   matrix M(var_num, sen_num);
   for (int i = 1; i <= sen_num; i++) {
     int tmp = 0;
 
     do {
-    std::cin >> tmp;
+      in >> tmp;
     if (tmp > 0)
       M.set_value(i, tmp, 1);
     if (tmp < 0) 
